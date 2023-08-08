8fe8c462b2061809b87732d868f9496611f48ffb
Pål Sola <bosen29@gmail.com>
2023-08-08 17:37:00 +0200

    added abilities to units

--- fetch.ps1
+++ fetch.ps1
@@ -12,7 +12,24 @@ if ($data.version) {
     $units = Invoke-RestMethod -Uri "$($baseuri)units/byVersion/$($data.version)?limit=250" -Headers $headers -Method GET
     $units += Invoke-RestMethod -Uri "$($baseuri)units/byVersion/$($data.version)?limit=250&offset=250" -Headers $headers -Method GET
     
+    $abilities = @()
+
+    # todo, implement proper calculations of the total amount of abilities... 
+    0..7 | ForEach-Object {
+        $abilities += Invoke-RestMethod -Uri "$($baseuri)info/abilities/$($_ * 50)/50" -Headers $headers -Method GET -ErrorAction SilentlyContinue
+    }
+
+    $abilities | ForEach-Object {
+        Set-Content -Path "$($_._id).json" -Value ($_ | ConvertTo-Json -Depth 4)
+    }
+
     $units | ForEach-Object {
+        $ab = $_.abilities | ConvertTo-Json | ConvertFrom-Json
+        $_.abilities = @()
+        foreach ($ability in $ab) {
+            Write-Host $abilitiy
+            $_.abilities += ($abilities | Where-Object {$_._id -eq $ability})
+        }
         Set-Content -path "$($_.unitId).json" -Value ($_ | ConvertTo-Json -Depth 4)
     }
 }
